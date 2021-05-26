5b9658476d4b3e32e68f6ba1972d07ea90db58f0
boatengdickson <dicksonboateng65@gmail.com>
2021-05-25 19:14:53 -0700

    Add parcel for building

--- js/dist/main.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach(function (item) {
-  return item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  });
-});
